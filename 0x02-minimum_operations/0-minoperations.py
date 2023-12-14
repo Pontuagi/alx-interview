85ece475004c71899482439778338a901faddceb
Pontuagi <benepontuagi@gmail.com>
2023-12-14 14:58:26 +0300

    Minimum operations corection

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -24,7 +24,8 @@ def minOperations(n):
     dp[1] = 0
 
     for i in range(2, n + 1):
-        for j in range(1, i // 2 + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    return dp[n] if dp[n] != float('inf') else 0
+        dp[i] = i
+        for j in range(1, i):
+            dp[i] = min(dp[i], dp[j] + dp[i - j] + 1)
+
+    return dp[n]
