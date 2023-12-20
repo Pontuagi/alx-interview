f5144fbd55e2baab9e4bb266f614382654698516
Pontuagi <benepontuagi@gmail.com>
2023-12-20 17:52:34 +0300

    Log parsing correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,34 +3,35 @@
 """
 This module contains a script that reads stdin line by line
 """
-
 import sys
 from collections import defaultdict
 
 
 def print_statistics(total_size, status_counts):
     """
-    Prints the computed statistics.
+    Print the computed statistics.
 
     Args:
     - total_size (int): Total file size computed from the input.
-    - status_counts (dict): Dictionary containing counts of status codes.
+    - status_counts (dict): Dictionary containing counts of specific status
+    codes.
     """
-    print(f"Total file size: File size: {total_size}")
+    print(f"File size: {total_size}")
     for code in sorted(status_counts.keys()):
         print(f"{code}: {status_counts[code]}")
 
 
 def process_lines(lines):
     """
-    Processes lines to compute total file size and status code counts.
+    Process lines to compute total file size and status code counts.
 
     Args:
     - lines (list): List of strings representing input lines.
 
     Returns:
     - total_size (int): Total file size computed from the input.
-    - status_counts (dict): Dictionary containing counts of status codes.
+    - status_counts (dict): Dictionary containing counts of specific status
+    codes.
     """
     total_size = 0
     status_counts = defaultdict(int)
@@ -39,10 +40,11 @@ def process_lines(lines):
         parts = line.split()
         if len(parts) >= 7:
             try:
-                status_code = int(parts[5])
-                file_size = int(parts[6])
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
                 total_size += file_size
-                status_counts[status_code] += 1
+                if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+                    status_counts[status_code] += 1
             except (ValueError, IndexError):
                 pass
 
@@ -51,9 +53,9 @@ def process_lines(lines):
 
 lines = []
 try:
-    for line in sys.stdin:
+    for i, line in enumerate(sys.stdin, start=1):
         lines.append(line.strip())
-        if len(lines) >= 10:
+        if i % 10 == 0:
             total_size, status_counts = process_lines(lines)
             print_statistics(total_size, status_counts)
             lines = []
