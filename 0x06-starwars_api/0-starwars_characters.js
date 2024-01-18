bf87d17105d62dcbd87ff6d16697708fe6b69a61
Pontuagi <benepontuagi@gmail.com>
2024-01-18 10:43:28 +0300

    semistandard error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,26 +9,26 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-if (movieId < 1 || movieId > 7){
-    console.log("Invalid Movie Id");
-    process.exit(1);
+if (movieId < 1 || movieId > 7) {
+  console.log('Invalid Movie Id');
+  process.exit(1);
 }
 request(url, async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-            const characterName = await new Promise((resolve, reject) => {
-                request(character, (error, response, body) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(JSON.parse(body).name);
-                    }
-                });
-            });
-            console.log(characterName);
-        }
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const characterName = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      console.log(characterName);
     }
-})
+  }
+});
