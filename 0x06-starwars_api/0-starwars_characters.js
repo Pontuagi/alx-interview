a27974c1880c69fc0989cf67862031c0433f9b4a
Pontuagi <benepontuagi@gmail.com>
2024-01-18 10:41:40 +0300

    Starwars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,4 +5,30 @@
  * A script that prints all characters of a Star Wars movie
  */
 
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+if (movieId < 1 || movieId > 7){
+    console.log("Invalid Movie Id");
+    process.exit(1);
+}
+request(url, async function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+            const characterName = await new Promise((resolve, reject) => {
+                request(character, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(JSON.parse(body).name);
+                    }
+                });
+            });
+            console.log(characterName);
+        }
+    }
+})
